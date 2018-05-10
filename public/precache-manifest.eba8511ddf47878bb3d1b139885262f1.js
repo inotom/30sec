1766ca7beecadbf9dd39b60c2c1ead096ee8afdd
inotom <wdf7322@yahoo.co.jp>
2018-05-10 13:34:26 +0900

    Add public directory

---
+++ public/precache-manifest.eba8511ddf47878bb3d1b139885262f1.js
@@ -0,0 +1,28 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fd2e3d146658a46f95f",
+    "url": "js/main.min.js"
+  },
+  {
+    "revision": "ad5d2793b4878024df28ed147048bb96",
+    "url": "index.html"
+  },
+  {
+    "url": "icon_96x96.019e3fd99e9e38404f1ee4e081419095.png"
+  },
+  {
+    "url": "icon_512x512.4bd8a8125ca3645884ac426e1be59b70.png"
+  },
+  {
+    "url": "icon_384x384.b051b451ba6b29c5cc5cb9c18f3b0a26.png"
+  },
+  {
+    "url": "icon_256x256.632baabe483ed660587cb6cb56268718.png"
+  },
+  {
+    "url": "icon_192x192.fe0a92d2cf5a75b029fe07629df3706b.png"
+  },
+  {
+    "url": "icon_128x128.61e6a1f830a0c0b95af2895fe1d1181d.png"
+  }
+];
---
+++ public/service-worker.js
@@ -0,0 +1,31 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.eba8511ddf47878bb3d1b139885262f1.js"
+);
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute("/", workbox.strategies.cacheFirst({ cacheName: "top-page-cache", plugins: [new workbox.expiration.Plugin({"maxAgeSeconds":1209600})] }), 'GET');
+workbox.routing.registerRoute(/\/js\/.+/, workbox.strategies.cacheFirst({ cacheName: "scripts-cache", plugins: [new workbox.expiration.Plugin({"maxAgeSeconds":1209600})] }), 'GET');
+workbox.routing.registerRoute(/\.(png|jpe?g|svg)/, workbox.strategies.cacheFirst({ cacheName: "assets", plugins: [new workbox.expiration.Plugin({"maxAgeSeconds":1209600})] }), 'GET');
